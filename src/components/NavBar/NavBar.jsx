b8ef468d7ce46b399d5c1918360943fb2c07cb5f
maxmay94 <maxmay94@gmail.com>
2022-08-17 17:22:15 -0400

    update navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,25 +2,25 @@ import { Link } from 'react-router-dom'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
+    <div className='text-xs md:text-md'>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
+        <nav className='bg-emerald-700'>
+          <ul className='flex p-2'>
+            <li className='flex-1 ml-5'><Link to="/profiles">Profiles</Link></li>
+            <li className='flex-1'><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+            <li className='flex-1'><Link to="/changePassword">Change Password</Link></li>
+            <li className='mr-5'>{user.name}</li>
           </ul>
         </nav>
       :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+        <nav className='bg-emerald-700'>
+          <ul className='flex p-2'>
+            <li className='flex-1 ml-5'><Link to="/login">Log In</Link></li>
+            <li className='mr-5'><Link to="/signup">Sign Up</Link></li>
           </ul>
         </nav>
       }
-    </>
+    </div>
   )
 }
 
